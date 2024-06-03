55d6d7e9b6e3b93780cb0063761750bc014f8e2a
jose-guerrerog <joseguerrerog880@gmail.com>
2024-06-04 06:50:55 +1000

    adding env variable

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Home } from './pages/Home';
 import { Chat } from './pages/Chat';
 
 import io from "socket.io-client";
-const socket = io.connect("http://localhost:4000");
+
+const socket = io.connect(process.env.REACT_APP_SERVER_URL);
 
 function App() {
   return (
